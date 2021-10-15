34ae3ccd3eff613e0ecdc7424092d777a32287f7
phvlkn <eminislamow@googlemail.com>
2021-10-15 22:14:46 +0400

    wall.get

---
+++ wallget.php
@@ -0,0 +1,15 @@
+<?php
+$method = 'wall.get';
+$token = $_POST['tkn'];
+$version = 5.131;
+
+$params = http_build_query([
+    'owner_id' => $_POST['id'],
+    'access_token' => $token,
+]);
+
+$url = "https://api.vk.com/method/{$method}?{$params}&access_token={$token}&v={$version}";
+$result = json_decode(file_get_contents($url), true);
+$orders = $result['response']['items'];
+print_r($result);
+?>
