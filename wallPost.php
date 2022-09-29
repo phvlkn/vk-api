fb78cba84658bd9b6bd7db31b45d911b74b578dc
phvlkn <eminislamow@googlemail.com>
2022-09-29 22:09:47 +0400

    добавить публикацию

---
+++ wallPost.php
@@ -0,0 +1,17 @@
+<?php
+$method = 'wall.post';
+$token = $_POST['tkn'];
+$version = 5.131;
+
+$params = http_build_query([
+    'owner_id' => $_POST['id'],
+    'friends_only' => $_POST['friends'],
+    'message' => $_POST['message'],
+    'attachments' => $_POST['attachments']
+]);
+
+$url = "https://api.vk.com/method/{$method}?{$params}&access_token={$token}&v={$version}";
+$result = json_decode(file_get_contents($url), true);
+$orders = $result['error']['items'];
+print_r($result);
+?>
